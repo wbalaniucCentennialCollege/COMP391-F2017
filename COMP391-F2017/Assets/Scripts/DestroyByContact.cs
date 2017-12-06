f69bdf8d4952b53bc8de2292e24aa5d4090415a8
wbalaniucCentennialCollege <wbalaniu@my.centennialcollege.ca>
2017-12-06 12:23:11 -0500

    Added asteroid animation

---
+++ COMP391-F2017/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    public GameObject explosionPlayer;
+    public GameObject explosion;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            Instantiate(explosionPlayer, other.gameObject.transform.position, other.gameObject.transform.rotation);
+        }
+
+        Instantiate(explosion, this.transform.position, this.transform.rotation);
+
+        Destroy(other.gameObject); // Destroying the "other" thing
+        Destroy(this.gameObject); // Destroying this thing
+    }
+}
---
+++ COMP391-F2017/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+
+	// Use this for initialization
+	public void DestroyObj () {
+        Destroy(this.gameObject);
+	}
+}
